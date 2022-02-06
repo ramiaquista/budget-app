c6d1b401ef2ff55818a1c97c44fa5f9acc4eaacf
Ramiro Aquistapace <ramiaquista70@gmail.com>
2022-02-06 12:54:15 -0300

    Generate expense and category scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :categories
+  resources :expenses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
