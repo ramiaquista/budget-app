f0fb80a04f2eb3e46cd8d0a23b5d8ddeea0fe5e3
Ramiro Aquistapace <ramiaquista70@gmail.com>
2022-02-06 15:31:31 -0300

    Add many to many relation between categories and expenses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_06_162525) do
+ActiveRecord::Schema.define(version: 2022_02_06_182638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 2022_02_06_162525) do
     t.string "icon"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_expenses", id: false, force: :cascade do |t|
+    t.bigint "categories_id"
+    t.bigint "expenses_id"
+    t.index ["categories_id"], name: "index_categories_expenses_on_categories_id"
+    t.index ["expenses_id"], name: "index_categories_expenses_on_expenses_id"
   end
 
   create_table "expenses", force: :cascade do |t|
@@ -55,6 +64,8 @@ ActiveRecord::Schema.define(version: 2022_02_06_162525) do
     t.float "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -72,4 +83,6 @@ ActiveRecord::Schema.define(version: 2022_02_06_162525) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "categories", "users"
+  add_foreign_key "expenses", "users"
 end
